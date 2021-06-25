b1015615845278f72a5d85db316c4a4facd0005f
LordBigglesworth <javanpohl@gmail.com>
2021-06-25 17:26:46 -0600

    got the stupid links to align at the bottom of the div in the header nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { styles } from './Header.module.css'
+import { header, li, nav, navUl, title, titleWhite } from './Header.module.css'
 
 const Header = () => {
-	return (
-		<div className={styles.header}>this is my header</div>
-	)
+  return (
+    <header className={header}>
+      <div className={title}><Link to='/'>FixNcell <span className={titleWhite}>Phone Repair</span></Link></div>
+        <nav className={nav}>
+          <ul className={navUl}>
+            <li className={li}>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className={li}>
+              <Link to='/about'>About</Link>
+            </li>
+            <li className={li}>
+              <Link to='/iphone-repair'>Repairs</Link>
+            </li>
+          </ul>
+        </nav>
+    </header>
+  )
 }
+
+export default Header
