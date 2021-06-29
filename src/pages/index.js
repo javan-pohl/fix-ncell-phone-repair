036b45635b999288a02703c36186ba80e8f0673d
LordBigglesworth <javanpohl@gmail.com>
2021-06-29 14:10:43 -0600

    fixed eslint issues with index page (they were fixed automatically)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/Layout/Layout'
 
-const IndexPage = () => {
-	return (
-		<Layout pageTitle="FixNcell Phone Repair Home Page">
-			<p>Test paragraph</p>
-		</Layout>
-	)
-}
+const IndexPage = () => (
+  <Layout pageTitle="FixNcell Phone Repair Home Page">
+    <p>Test paragraph</p>
+  </Layout>
+)
 
 export default IndexPage
