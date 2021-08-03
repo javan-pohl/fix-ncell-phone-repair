02a0f3781d0b2e73e3643eff72bed891245f7449
LordBigglesworth <javanpohl@gmail.com>
2021-08-03 10:43:47 -0600

    pulling in data into the phone template page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,41 @@
-const { createFilePath } = require('gatsby-source-filesystem')
+// const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === 'PhonesJson') {
-    console.log('onCreateNode: ', node.make, node.model)
-    // const slug = createFilePath({ node, getNode, basePath: 'repairs' })
-    const slug = `/repairs/${node.make}-${node.model}/`.replace(' ', '-').toLowerCase()
-    console.log('slug: ', slug)
-    createNodeField({
-      node,
-      name: 'slug',
-      value: slug,
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+//   if (node.internal.type === 'PhonesJson') {
+//     console.log('onCreateNode: ', node.make, node.model)
+//     // const slug = createFilePath({ node, getNode, basePath: 'repairs' })
+//     const slug = `/repairs/${node.make}-${node.model}/`
+//       .replace(' ', '-')
+//       .toLowerCase()
+//     console.log('slug: ', slug)
+//     createNodeField({
+//       node,
+//       name: 'slug',
+//       value: slug,
+//     })
+//   }
+// }
+
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allPhonesJson {
+        nodes {
+          make
+          model
+        }
+      }
+    }
+  `)
+  data.allPhonesJson.nodes.forEach((node) => {
+    const slug = `/repairs/${node.make}-${node.model}/`
+      .replace(' ', '-')
+      .toLowerCase()
+    actions.createPage({
+      path: slug,
+      component: require.resolve('./src/pages/templates/phones.js'),
+      context: { slug },
     })
-  }
+  })
 }
--- src/pages/templates/phones.js
+++ src/pages/templates/phones.js
@@ -1,9 +1,28 @@
 import * as React from 'react'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout/Layout'
 
-const PhonePage = () => (
-  <Layout pageTitle="Phone Page">
-    <p>This is my default phone page</p>
-  </Layout>
-)
+const PhonePage = ({ data }) => {
+  console.log('data: ', data)
+  return (
+    <Layout pageTitle="Phone Page">
+      <h2>This is my default phone page</h2>
+      <h3>
+        {data.phonesJson.make}
+        {' '}
+        {data.phonesJson.model}
+      </h3>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query phoneQuery($id: String) {
+    phonesJson(id: { eq: $id }) {
+      make
+      model
+    }
+  }
+`
+
 export default PhonePage
