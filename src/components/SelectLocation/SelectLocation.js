5c00bdd09fd7b69abe31a79feddf1164bb17ea95
LordBigglesworth <javanpohl@gmail.com>
2021-09-28 15:50:02 -0600

    have started a select location section for the main page

---
+++ src/components/SelectLocation/SelectLocation.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { locationsMain, locsDiv, locCard } from './SelectLocation.module.css'
+import getLocations from '../../queries/getLocations/getLocations'
+
+function SelectLocation() {
+  const { locations } = getLocations()
+  const locArray = locations.map((location) => location.name)
+  return (
+    <div className={locationsMain}>
+      <h3>Select Your Location</h3>
+      <div className={locsDiv}>
+        {locArray.map((loc) => (
+          <Link to={`/${loc.toLowerCase()}/repairs`} key={loc}>
+            <div className={locCard}>{loc}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SelectLocation
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/Layout/Layout'
 import Showcase from '../components/Showcase/Showcase'
+import SelectLocation from '../components/SelectLocation/SelectLocation'
+
 /* eslint-disable react/prop-types */
 
 const IndexPage = ({ location: { pathname } }) => (
   <Layout pageTitle="FixNcell Phone Repair Home Page" currentUrl={pathname}>
     <Showcase />
+    <SelectLocation />
     <p>Test paragraph</p>
   </Layout>
 )
