47efa121d3d526a791d9eb42173731b92e1f15d5
LordBigglesworth <javanpohl@gmail.com>
2021-10-01 16:42:32 -0600

    added the other review widget (from embedsocial)

---
+++ src/components/EmbedSocialWidget/EmbedSocialWidget.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class EmbedSocialWidget extends React.Component {
+  render() {
+    return <div className="embedsocial-reviews" data-ref={this.props.refId} />
+  }
+
+  componentDidMount() {
+    (function (d, s, id) {
+      let js
+      if (d.getElementById(id)) {
+        return
+      }
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://embedsocial.com/embedscript/ri.js'
+      d.getElementsByTagName('head')[0].appendChild(js)
+    }(document, 'script', 'EmbedSocialReviewsScript'))
+  }
+}
+
+export default EmbedSocialWidget
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,22 @@ import Layout from '../components/Layout/Layout'
 import Showcase from '../components/Showcase/Showcase'
 import SelectLocation from '../components/SelectLocation/SelectLocation'
 import useScript from '../hooks/useScript'
+import EmbedSocialWidget from '../components/EmbedSocialWidget/EmbedSocialWidget'
 
 /* eslint-disable react/prop-types */
 
-const IndexPage = ({ location: { pathname } }) => {
-  useScript('https://reviewsonmywebsite.com/js/embedLoader.js?id=cb477ad3a93eb5d679f0')
-  return (
+const IndexPage = ({ location: { pathname } }) =>
+  // useScript('https://reviewsonmywebsite.com/js/embedLoader.js?id=cb477ad3a93eb5d679f0')
+  (
     <Layout pageTitle="FixNcell Phone Repair Home Page" currentUrl={pathname}>
       <Showcase />
       <SelectLocation />
       <p>Test paragraph</p>
-      <div
-        data-token="t6Oio5DNjj3udVGPHeuzdMKTc0wPM7wU6FuGZlB8aeTB9RoYjv"
-        className="romw-reviews"
-      />
 
+      <div>
+        <EmbedSocialWidget refId="290bdda409a96a5c382887408438f18ddeabb3ab" />
+      </div>
     </Layout>
   )
-}
 
 export default IndexPage
