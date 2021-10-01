ebe8a4a0cdfea966e26d7b3cefaa88d67352fc8e
LordBigglesworth <javanpohl@gmail.com>
2021-10-01 15:06:48 -0600

    testing out the review component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,24 @@ import * as React from 'react'
 import Layout from '../components/Layout/Layout'
 import Showcase from '../components/Showcase/Showcase'
 import SelectLocation from '../components/SelectLocation/SelectLocation'
+import useScript from '../hooks/useScript'
 
 /* eslint-disable react/prop-types */
 
-const IndexPage = ({ location: { pathname } }) => (
-  <Layout pageTitle="FixNcell Phone Repair Home Page" currentUrl={pathname}>
-    <Showcase />
-    <SelectLocation />
-    <p>Test paragraph</p>
-  </Layout>
-)
+const IndexPage = ({ location: { pathname } }) => {
+  useScript('https://reviewsonmywebsite.com/js/embedLoader.js?id=cb477ad3a93eb5d679f0')
+  return (
+    <Layout pageTitle="FixNcell Phone Repair Home Page" currentUrl={pathname}>
+      <Showcase />
+      <SelectLocation />
+      <p>Test paragraph</p>
+      <div
+        data-token="t6Oio5DNjj3udVGPHeuzdMKTc0wPM7wU6FuGZlB8aeTB9RoYjv"
+        className="romw-reviews"
+      />
+
+    </Layout>
+  )
+}
 
 export default IndexPage
