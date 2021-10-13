9717db666845893da8ed149e863412cde084d938
LordBigglesworth <javanpohl@gmail.com>
2021-10-13 14:37:11 -0600

    changed background image in showcase to use mobile friendly version

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -10,18 +10,37 @@ import {
 import MyLink from '../MyLink/MyLink'
 import ReviewBadges from '../Badges/ReviewBadges'
 
+const BackImg = {
+  margin: '0px',
+  display: 'inlineBlock',
+  paddingTop: '0px',
+  background: `url(\'https://www.fixncell.com/img/mobile_iphone_screen_repair_bw.jpg\')
+	noRepeat 0 -70px'`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'bottom',
+  height: '300px',
+  width: '100%',
+  textAlign: 'center,',
+}
+
 function Showcase() {
   return (
-    <section className={showcaseMobileSection}>
+    <section
+      className={showcaseMobileSection}
+    >
       <div className={showcaseMobile}>
         <div className={showcaseMobileInner}>
           <span id={showcaseMobileHeader}>LET US COME TO YOU!</span>
           <p>FixNcell now comes to your location to fix your phone</p>
         </div>
-        <Link to="/schedule-your-repair" className={showcaseMobileScheduleButton}>Schedule now</Link>
+        <Link
+          to="/schedule-your-repair"
+          className={showcaseMobileScheduleButton}
+        >
+          Schedule now
+        </Link>
       </div>
       <ReviewBadges />
-
     </section>
   )
 }
