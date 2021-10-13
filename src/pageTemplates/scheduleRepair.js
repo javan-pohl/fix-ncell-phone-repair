0e55ae5521f5429833ddfab9d78cc15547eefed7
LordBigglesworth <javanpohl@gmail.com>
2021-10-13 12:54:59 -0600

    made the div around the accuity iframe have a height of 100% to prevent double scrolling

--- src/pageTemplates/scheduleRepair.js
+++ src/pageTemplates/scheduleRepair.js
@@ -21,17 +21,19 @@ const ScheduleRepair = ({
       {' '}
       {location}
     </h1>
-    <iframe
-      src={`https://app.acuityscheduling.com/schedule.php?owner=11997785&appointmentType=${screenScheduleID}`}
-      title="Schedule Appointment"
-      width="100%"
-      height="800"
-      frameBorder="0"
-    />
-    <script
-      src="https://embed.acuityscheduling.com/js/embed.js"
-      type="text/javascript"
-    />
+    <div style={{ height: '100%' }}>
+      <iframe
+        src={`https://app.acuityscheduling.com/schedule.php?owner=11997785&appointmentType=${screenScheduleID}`}
+        title="Schedule Appointment"
+        width="100%"
+        height="800"
+        frameBorder="0"
+      />
+      <script
+        src="https://embed.acuityscheduling.com/js/embed.js"
+        type="text/javascript"
+      />
+    </div>
   </Layout>
 )
 
