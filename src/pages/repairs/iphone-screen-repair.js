653dfb592806ad51725f0ddc33169633cfc6c8dd
LordBigglesworth <javanpohl@gmail.com>
2021-10-28 17:33:27 -0600

    changed screen repair photo to black and white

--- src/pages/repairs/iphone-screen-repair.js
+++ src/pages/repairs/iphone-screen-repair.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import Layout from '../../components/Layout/Layout'
 import useWindowSize from '../../hooks/useWindowSize'
-import phoneRepairSm from '../../images/iphone_screen_repair_439.webp'
+import phoneRepairSm from '../../images/iphone_screen_repair_439_b&w.webp'
 import { fadeText } from './iphone-screen-repair.module.css'
 
 const mainBackImg = (
@@ -58,7 +58,7 @@ const aboutHeading = {
   lineHeight: '10vw',
   fontFamily: '"Times New Roman", Times, serif',
   fontSize: '6vw',
-  backgroundColor: 'rgba(0,0,0,.5)',
+  backgroundColor: 'rgba(0,0,0,.8)',
   border: '2px solid white',
   display: 'flex',
   justifyContent: 'center',
@@ -77,7 +77,7 @@ const iPhoneScreenRepair = ({ location: { pathname } }) => {
         />
         <title>{title}</title>
       </Helmet>
-      <div style={mainBackImg(phoneRepairSm, 0.95, '0,0,0')}>
+      <div style={mainBackImg(phoneRepairSm, 0.6, '0,0,0')}>
         <span
           role="img"
           aria-label="An iPhone X that is in the process of having its screen replaced. The phone is open and the various tools used in the repair are located on the table next to it."
