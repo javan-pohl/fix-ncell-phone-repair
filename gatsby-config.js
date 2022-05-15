4c79852648836fb947a13872ef421c5a7573365c
LordBigglesworth <javanpohl@gmail.com>
2022-05-15 17:28:38 -0600

    did i delete the tracking code from gtag? added back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          '', // Google Analytics / GA
+          'UA-70371516-1', // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
