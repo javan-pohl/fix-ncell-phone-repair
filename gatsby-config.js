e37d95234c68c69cc1e0e8f265761cbac55f91a5
LordBigglesworth <javanpohl@gmail.com>
2022-05-15 16:49:38 -0600

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,31 +39,6 @@ module.exports = {
     siteUrl: 'https://www.fixncell.com',
   },
   plugins: [
-    'gatsby-plugin-robots-txt',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-next-seo',
-    {
-      resolve: 'gatsby-plugin-scroll-reveal',
-      options: {
-        threshold: 0.1, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
-
-        // Advanced Options
-        selector: '[data-sal]', // Selector of the elements to be animated
-        animateClassName: 'sal-animate', // Class name which triggers animation
-        disabledClassName: 'sal-disabled', // Class name which defines the disabled state
-        rootMargin: '0% 50%', // Corresponds to root's bounding box margin
-        enterEventName: 'sal:in', // Enter event name
-        exitEventName: 'sal:out', // Exit event name
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
@@ -94,6 +69,31 @@ module.exports = {
         enableWebVitalsTracking: true,
       },
     },
+    'gatsby-plugin-robots-txt',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-next-seo',
+    {
+      resolve: 'gatsby-plugin-scroll-reveal',
+      options: {
+        threshold: 0.1, // Percentage of an element's area that needs to be visible to launch animation
+        once: true, // Defines if animation needs to be launched once
+        disable: false, // Flag for disabling animations
+
+        // Advanced Options
+        selector: '[data-sal]', // Selector of the elements to be animated
+        animateClassName: 'sal-animate', // Class name which triggers animation
+        disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+        rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+        enterEventName: 'sal:in', // Enter event name
+        exitEventName: 'sal:out', // Exit event name
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-manifest',
