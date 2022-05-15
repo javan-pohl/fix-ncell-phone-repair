40e026e9d58e8d415f7ef0d9f265a1f7c26f2c06
LordBigglesworth <javanpohl@gmail.com>
2022-05-15 17:18:47 -0600

    removed unused elements from gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,13 +45,10 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           'UA-70371516-1', // Google Analytics / GA
-          'AW-CONVERSION_ID', // Google Ads / Adwords / AW
-          'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: 'OPT_CONTAINER_ID',
           anonymize_ip: true,
           cookie_expires: 0,
         },
@@ -62,7 +59,6 @@ module.exports = {
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
-          exclude: ['/preview/**', '/do-not-track/me/too/'],
         },
       },
     },
